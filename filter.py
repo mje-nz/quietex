723d0b502399dffbc1676671f60c862ddecc9273
Matthew Edwards <mje-nz@users.noreply.github.com>
2019-07-30 14:53:08 +1200

    Handle input prompt after errors

--- filter.py
+++ filter.py
@@ -15,14 +15,59 @@ from colorama import Fore, Style
 colorama.init()
 
 
+def probably_warning(line):
+    if line.startswith('Overfull') or line.startswith('Underfull'):
+        return True
+    line = line.lower()
+    if 'warning' in line or 'missing' in line:
+        return True
+    if 'undefined' in line and "i'll" not in line:
+        # Check for "I'll forget about whatever was undefined." in help text
+        return True
+    return False
+
+
+def handle_prompt(pdflatex):
+    if pdflatex.buffer != '? ':
+        # It's not waiting for input
+        return
+    try:
+        prompt = pdflatex.read(2)
+    except pexpect.exceptions.TIMEOUT:
+        # It's not waiting for input
+        return
+    while True:
+        # pdflatex needs a newline after Ctrl+C, so loop until we get a proper
+        # response from the user
+        try:
+            user_response = input(Fore.RED + prompt + Style.RESET_ALL)
+            pdflatex.send(user_response + '\n')
+            return
+        except EOFError:
+            # Ctrl+D at input prompt (pdflatex responds immediately)
+            pdflatex.sendcontrol('d')
+            return
+        except KeyboardInterrupt:
+            # Ctrl+C at input prompt (pdflatex responds at end of line)
+            pdflatex.sendintr()
+            prompt = ''
+
+
 def main(cmd):
     # Disable pdflatex line wrap (where possible)
     env = dict(os.environ, max_print_line="1000000000")
 
     # Run pdflatex and filter/colour output
-    pdflatex = pexpect.spawn(cmd[0], cmd[1:], env=env, encoding='utf-8')
+    pdflatex = pexpect.spawn(
+        cmd[0], cmd[1:], env=env, encoding='utf-8', timeout=0.2
+    )
     while True:
-        line = pdflatex.readline()
+        try:
+            line = pdflatex.readline()
+        except pexpect.exceptions.TIMEOUT:
+            # Check if it's waiting for input
+            handle_prompt(pdflatex)
+            continue
         if line == '':
             # EOF
             break
@@ -38,15 +83,13 @@ def main(cmd):
         elif line.startswith('!'):
             # Error
             print(Style.BRIGHT + Fore.RED + line + Style.RESET_ALL)
-        elif line.startswith('Overfull') or line.startswith('Underfull') \
-                or 'warning' in line.lower() or 'missing' in line.lower() or \
-                'undefined' in line.lower():
-            # Warning
+        elif probably_warning(line):
             print(Fore.YELLOW + line + Style.RESET_ALL)
         else:
             print(line)
 
-    # TODO: Handle input prompt when there's an error
+    pdflatex.close()
+    sys.exit(pdflatex.exitstatus)
 
 
 if __name__ == '__main__':
